895855afb4f17883f1ae1d2ad15b37900f974a5c
annguyen <ankentino123@gmail.com>
2025-02-15 14:22:43 +0700

    Cap Nhat form 1

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -34,6 +34,7 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.cong = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -84,11 +85,22 @@
             this.textBox3.Size = new System.Drawing.Size(100, 20);
             this.textBox3.TabIndex = 5;
             // 
+            // cong
+            // 
+            this.cong.Location = new System.Drawing.Point(395, 236);
+            this.cong.Name = "cong";
+            this.cong.Size = new System.Drawing.Size(75, 23);
+            this.cong.TabIndex = 6;
+            this.cong.Text = "cong";
+            this.cong.UseVisualStyleBackColor = true;
+            this.cong.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1097, 429);
+            this.Controls.Add(this.cong);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
@@ -110,6 +122,7 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button cong;
     }
 }
 
